6fde049462590d892e50600ca82d7fa7fc2e04bd
Kateryna Tymofieieva <katatimofeeva82@gmail.com>
2024-01-07 20:45:54 -0800

    promis.all wrapper create

--- handler/actionHandler/parallel.ts
+++ handler/actionHandler/parallel.ts
@@ -1,16 +1,25 @@
 import { IParallelAction } from "_config/config.handler";
 import { TCreateActionHandler } from "./index";
 
-const parallelActionHandler: TCreateActionHandler<IParallelAction> = (actions: {
+const parallelActionHandler: any = (actions: {
     type: 'select',
     entityName: 'product' | 'client' | 'invoice' ,
     where: [string, string],
     assignVar: string,
   }[]) => {
-    return {
-        type: 'parallel',
-        actions
-    }
+    return `
+    async function(){
+    const result = await Promise.all(${actions}.map(async (action) => {
+      const data = await dataSource
+          .createQueryBuilder()
+          .select('*')
+          .from(action.entityName)
+          .where(action.where[0] = :value, { value: action.where[1] })
+          .getOne();
+          console.log(data, 'data')
+      return data;
+  }));
+}`
 }
 
 export default parallelActionHandler;
