43da2857347002d1261a7613ba8dada09b0da53c
Andrew1326 <maxgreen007007@gmail.com>
2024-01-15 13:12:12 +0000

    upd

--- handler/actionHandler/delete.ts
+++ handler/actionHandler/delete.ts
@@ -1,18 +1,21 @@
-import {IDeleteAction} from '_config/config.handler'
-import { createClassName } from 'helpers';
+import { IDeleteAction } from '_config/config.handler';
+import { createClassName } from '../../helpers';
 import { TCreateActionHandler } from './index';
 
 const deleteActionHandler: TCreateActionHandler<IDeleteAction> = ({
   entityName,
   where,
 }) => {
+  const field = where[0];
+  const value = where[1].replaceAll('$', '');
+
   const entityClassName = createClassName(entityName);
 
-  const entries = `await myDataSource
+  const entries = `await dataSource
     .createQueryBuilder()
     .delete()
     .from(entities.${entityClassName}) 
-    .where("${where[0]} = :${where[0]}", { ${[where[0]]}: ${where[1]} })
+    .where('${field} = :${field}', { ${field}: ${value} })
     .execute()`;
 
   return entries;
