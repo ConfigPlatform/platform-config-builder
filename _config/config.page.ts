06e640a538d0449fc0a37e35bc1f07a6c5ed5cba
GerelBudaeva <gerel.budaeva@gmail.com>
2024-01-26 21:04:00 -0800

    changed classes

--- _config/config.page.ts
+++ _config/config.page.ts
@@ -78,20 +78,20 @@ const pages: IPages = {
           'md:w-2/5 w-full flex items-center justify-center py-5 lg:px-0 sm:px-6 px-4',
         previousButton: {
           className:
-            'text-sm font-medium cursor-pointer text-gray-600 bg-gray-100 hover:text-indigo-700 border border-e-0 border-gray-300 p-2 rounded-s-md',
+            'flex items-center justify-center px-3 h-8 ms-0 leading-tight text-gray-500 bg-white border border-e-0 border-gray-300 rounded-s-lg hover:bg-gray-100 hover:text-gray-700 dark:bg-gray-800 dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white',
         },
         nextButton: {
           className:
-            'text-sm font-medium cursor-pointer text-gray-600 bg-gray-100 hover:text-indigo-700 border border-gray-300 p-2 rounded-e-md',
+            'flex items-center justify-center px-3 h-8 ms-0 leading-tight text-gray-500 bg-white border border-gray-300 rounded-e-lg hover:bg-gray-100 hover:text-gray-700 dark:bg-gray-800 dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white',
         },
         pagesWrapper: { className: 'flex items-center' },
         page: {
           className:
-            'text-sm font-medium cursor-pointer text-gray-600 bg-gray-100 hover:text-indigo-700 hover:border-indigo-400 border border-e-0 border-gray-300 p-2',
+            'flex items-center justify-center px-3 h-8 leading-tight text-gray-500 bg-white border border-e-0 border-gray-300 hover:bg-gray-100 hover:text-gray-700 dark:bg-gray-800 dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white',
         },
         pageActive: {
           className:
-            'text-sm font-medium cursor-pointer text-gray-600 bg-gray-300 hover:text-indigo-700 hover:border-indigo-400 border border-e-0 border-gray-300 p-2',
+            'flex items-center justify-center px-3 h-8 text-blue-600 border border-e-0 border-gray-300 bg-blue-50 hover:bg-blue-100 hover:text-blue-700 dark:border-gray-700 dark:bg-gray-700 dark:text-white',
         },
       },
     ],
@@ -245,20 +245,20 @@ const pages: IPages = {
           'md:w-2/5 w-full flex items-center justify-center py-5 lg:px-0 sm:px-6 px-4',
         previousButton: {
           className:
-            'text-sm font-medium cursor-pointer text-gray-600 bg-gray-100 hover:text-indigo-700 border border-e-0 border-gray-300 p-2 rounded-s-md',
+            'flex items-center justify-center px-3 h-8 ms-0 leading-tight text-gray-500 bg-white border border-e-0 border-gray-300 rounded-s-lg hover:bg-gray-100 hover:text-gray-700 dark:bg-gray-800 dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white',
         },
         nextButton: {
           className:
-            'text-sm font-medium cursor-pointer text-gray-600 bg-gray-100 hover:text-indigo-700 border border-gray-300 p-2 rounded-e-md',
+            'flex items-center justify-center px-3 h-8 ms-0 leading-tight text-gray-500 bg-white border border-gray-300 rounded-e-lg hover:bg-gray-100 hover:text-gray-700 dark:bg-gray-800 dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white',
         },
         pagesWrapper: { className: 'flex items-center' },
         page: {
           className:
-            'text-sm font-medium cursor-pointer text-gray-600 bg-gray-100 hover:text-indigo-700 hover:border-indigo-400 border border-e-0 border-gray-300 p-2',
+            'flex items-center justify-center px-3 h-8 leading-tight text-gray-500 bg-white border border-e-0 border-gray-300 hover:bg-gray-100 hover:text-gray-700 dark:bg-gray-800 dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white',
         },
         pageActive: {
           className:
-            'text-sm font-medium cursor-pointer text-gray-600 bg-gray-300 hover:text-indigo-700 hover:border-indigo-400 border border-e-0 border-gray-300 p-2',
+            'flex items-center justify-center px-3 h-8 text-blue-600 border border-e-0 border-gray-300 bg-blue-50 hover:bg-blue-100 hover:text-blue-700 dark:border-gray-700 dark:bg-gray-700 dark:text-white',
         },
       },
     ],
@@ -448,20 +448,20 @@ const pages: IPages = {
           'md:w-2/5 w-full flex items-center justify-center py-5 lg:px-0 sm:px-6 px-4',
         previousButton: {
           className:
-            'text-sm font-medium cursor-pointer text-gray-600 bg-gray-100 hover:text-indigo-700 border border-e-0 border-gray-300 p-2 rounded-s-md',
+            'flex items-center justify-center px-3 h-8 ms-0 leading-tight text-gray-500 bg-white border border-e-0 border-gray-300 rounded-s-lg hover:bg-gray-100 hover:text-gray-700 dark:bg-gray-800 dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white',
         },
         nextButton: {
           className:
-            'text-sm font-medium cursor-pointer text-gray-600 bg-gray-100 hover:text-indigo-700 border border-gray-300 p-2 rounded-e-md',
+            'flex items-center justify-center px-3 h-8 ms-0 leading-tight text-gray-500 bg-white border border-gray-300 rounded-e-lg hover:bg-gray-100 hover:text-gray-700 dark:bg-gray-800 dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white',
         },
         pagesWrapper: { className: 'flex items-center' },
         page: {
           className:
-            'text-sm font-medium cursor-pointer text-gray-600 bg-gray-100 hover:text-indigo-700 hover:border-indigo-400 border border-e-0 border-gray-300 p-2',
+            'flex items-center justify-center px-3 h-8 leading-tight text-gray-500 bg-white border border-e-0 border-gray-300 hover:bg-gray-100 hover:text-gray-700 dark:bg-gray-800 dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white',
         },
         pageActive: {
           className:
-            'text-sm font-medium cursor-pointer text-gray-600 bg-gray-300 hover:text-indigo-700 hover:border-indigo-400 border border-e-0 border-gray-300 p-2',
+            'flex items-center justify-center px-3 h-8 text-blue-600 border border-e-0 border-gray-300 bg-blue-50 hover:bg-blue-100 hover:text-blue-700 dark:border-gray-700 dark:bg-gray-700 dark:text-white',
         },
       },
     ],
