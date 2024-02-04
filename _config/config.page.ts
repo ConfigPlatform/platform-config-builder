bdf809742ace9cf1250a14c51242da92063085bd
Andrew1326 <maxgreen007007@gmail.com>
2024-02-04 17:50:10 +0000

    upd

--- _config/config.page.ts
+++ _config/config.page.ts
@@ -288,20 +288,6 @@ const pages: IPages = {
                   className:
                     'px-5 py-5 border-b border-gray-200 bg-white text-sm',
                   content: [
-                    // {
-                    //   type: 'button',
-                    //   label: 'Edit',
-                    //   className:
-                    //     'text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-xs px-3 py-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800 me-2',
-                    //   serverHandler: '',
-                    // },
-                    // {
-                    //   type: 'button',
-                    //   label: 'Delete',
-                    //   className:
-                    //     'focus:outline-none text-white bg-red-700 hover:bg-red-800 focus:ring-4 focus:ring-red-300 font-medium rounded-lg text-xs px-3 py-2 dark:bg-red-600 dark:hover:bg-red-700 dark:focus:ring-red-900',
-                    //   serverHandler: '',
-                    // },
                     {
                       type: 'dropdown',
                       title: 'select action',
@@ -320,7 +306,6 @@ const pages: IPages = {
                           label: 'Delete',
                           className:
                             'focus:outline-none text-white bg-red-700 hover:bg-red-800 focus:ring-4 focus:ring-red-300 font-medium rounded-lg text-xs px-3 py-2 dark:bg-red-600 dark:hover:bg-red-700 dark:focus:ring-red-900',
-                          
                         },
                       ]  
                     }
