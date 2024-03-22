f985dcdd829dc12d87dd6341daa454808337d049
Andrew1326 <maxgreen007007@gmail.com>
2024-03-22 11:06:09 +0000

    upd

--- handler/actionHandler/variableUpdate.ts
+++ handler/actionHandler/variableUpdate.ts
@@ -1,5 +1,6 @@
 import { IVariableUpdateAction } from '_config/types/config.handler';
 import { TCreateActionHandler } from './index';
+import { createValueFromTemplate } from '../../helpers';
 
 const variableUpdateActionHandler: TCreateActionHandler<
   IVariableUpdateAction
